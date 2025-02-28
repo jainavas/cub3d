79073af396642dcdc5df4715e53031e010f74870
jaime <jaimeipod99@gmail.com>
2025-02-28 16:59:39 +0100

    done

--- map/parser_utils.c
+++ map/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: jainavas <jainavas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:09:49 by mhiguera          #+#    #+#             */
-/*   Updated: 2025/02/27 02:04:48 by jainavas         ###   ########.fr       */
+/*   Updated: 2025/02/28 16:58:43 by jainavas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,8 @@ int	parse_map_content(t_map *vmap, int mapstart, int fd, int nred)
 			if (get_textures_paths(vmap, line))
 				return (ft_putstr_fd("Error\nTextura o color incorrecto\n", 1),
 					freepaths(vmap), free(line), 1);
-		if (mapstart && !is_map_line(line))
+		if ((mapstart && !is_map_line(line)) || (!is_map_line(line)
+			&& !is_texture_or_color(line) && line[0] != '\n'))
 			return (ft_putstr_fd("Error\nLinea de mapa incorrecta\n", 1),
 				freepaths(vmap), free(line), 1);
 		if (is_map_line(line))
